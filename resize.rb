74b64963234029000a5fbef42607452762d4cb2f
jinlei04 <jinlei04@baidu.com>
2015-04-09 19:35:30 +0800

    ok

--- resize.rb
+++ resize.rb
@@ -8,13 +8,13 @@ Dir['images/**/*'].each do |file|
         next
     end
 
-    unless /(?<filename>.+)\.JPG/ =~ file
+    unless /(?<filename>.+)\.(?<ext>(JPG|jpg))/ =~ file
         next
     end
 
 #     puts "#{file}"
 #     puts "#{filename}"
-    system "convert #{filename}.JPG -resize 1080x1080 #{filename}.JPG"
+    system "convert #{filename}.#{ext} -resize 1080x1080 #{filename}.#{ext}"
 
 end
 
